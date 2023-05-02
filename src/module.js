ad9168c5645071936b09846fe6d253083f201dbf
Vladimir <vovagarmonshikov@mail.ru>
2023-05-02 18:15:59 +0300

    fix bags 1.9

--- src/module.js
+++ src/module.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
-export const isCheckRound = (theAnswer, enteredResponse, name, roundtimer) => {
+
+ const isCheckRound = (theAnswer, enteredResponse, name, roundtimer) => {
   let result = true;
   if (roundtimer >= 3 && theAnswer === enteredResponse) {
-    console.log('Correct!');
+    console.log("Correct!");
     console.log(`Congratulations, ${name}!`);
     return result;
   }
   if (theAnswer === enteredResponse) {
-    console.log('Correct!');
+    console.log("Correct!");
     return result;
   }
   result = false;
   console.log(
-    `'${enteredResponse}' is wrong answer ;(. Correct answer was '${theAnswer}'.`,
+    `'${enteredResponse}' is wrong answer ;(. Correct answer was '${theAnswer}'.`
   );
   console.log(`Let's try again, ${name}!`);
   return result;
 };
+export default isCheckRound;
